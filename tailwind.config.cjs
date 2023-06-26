f208f3414ea01d5c2227d5f4fac25685c1efa1bb
JudetheGemini <jude58005@gmail.com>
2023-06-26 16:38:25 +0100

    progress on home page

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    colors: {
+      darkBlue: "#263238",
+    },
+    fontSize: {
+      xs: "0.6rem",
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+  darkMode: "class",
+};
