b907963248e9c2df537dc277bfc5d1683e574db6
JudetheGemini <jude58005@gmail.com>
2023-10-04 15:15:54 +0100

    added contact and services pages. changed hero images from png to svg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,7 +21,45 @@ module.exports = {
     },
     lineHeight: { 12: "3rem" },
     fontFamily: { kumbh: ['"Kumbh Sans"', "sans-serif"] },
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          "5%, 50%": {
+            transform: "scale(1)",
+          },
+
+          "10%": {
+            transform: "scale(1)",
+          },
+
+          "15%": {
+            transform: "scale(1)",
+          },
+
+          "20%": {
+            transform: "scale(1) rotate(-5deg)",
+          },
+
+          "25%": {
+            transform: "scale(1) rotate(5deg)",
+          },
+
+          "30%": {
+            transform: "scale(1) rotate(-3deg)",
+          },
+
+          "35%": {
+            transform: "scale(1) rotate(2deg)",
+          },
+          "40%": {
+            transform: "scale(1) rotate(0)",
+          },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 4s infinite",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
   darkMode: "class",
