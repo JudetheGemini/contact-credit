3caac16c5e40d38f1819c470534abbcc45dc1c8b
JudetheGemini <jude58005@gmail.com>
2023-10-06 12:19:47 +0100

    work resumes friday, on a mission to complete homepage

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require(nodemailer);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
